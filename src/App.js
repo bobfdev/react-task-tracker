0476abc0a508befac102bddc6abd79b4e7008150
Bob F <bobfdev@gmail.com>
2021-11-15 16:11:58 -0600

    updated App.js file and added AddTask.js file to components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import React from 'react';
+
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
     const [tasks, setTasks] = useState([
@@ -25,6 +26,13 @@ function App() {
       },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,6 +48,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks 
         tasks={tasks} 
